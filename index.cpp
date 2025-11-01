56b675e1eafb2c6f68e809e80f60cb9cb80978a5
Shivam-Anand-03 <shivam850anand@gmail.com>
2025-11-01 23:25:40 +0530

    fixed

--- index.cpp
+++ index.cpp
@@ -35,7 +35,7 @@ int lastOccurence(vector<int> &nums, int target)
     if (nums[mid] == target)
     {
       occ = mid;
-      left = mid + 1;
+      left = mid + 1; // keep searching right side
     }
     else if (nums[mid] < target)
     {
@@ -49,16 +49,20 @@ int lastOccurence(vector<int> &nums, int target)
   return occ;
 }
 
-vector<int> searchRange(vector<int> &nums, int target)
+int countOccurrences(vector<int> &arr, int target)
 {
-  return {firstOccurence(nums, target), lastOccurence(nums, target)};
+  int first = firstOccurence(arr, target);
+  int last = lastOccurence(arr, target);
+  if (first == -1 || last == -1)
+    return 0;
+  return (last - first + 1);
 }
 
 int main()
 {
-  vector<int> nums = {5, 7, 7, 8, 8, 10};
-  int target = 8;
-  vector<int> result = searchRange(nums, target);
-  cout << "FIRST OCC : " << result[0] << ", LAST OCC : " << result[1] << endl;
+  vector<int> nums = {0, 0, 1, 1, 1, 2, 3};
+  int target = 1;
+  int result = countOccurrences(nums, target);
+  cout << result;
   return 0;
 }
